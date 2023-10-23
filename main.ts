da5331f1f46432850210fe0b900e68c3d2757957
MiniReyux <ja.reyes2@uniandes.edu.co>
2023-10-22 22:38:21 -0500

    commit

--- main.ts
+++ main.ts
@@ -2,12 +2,16 @@ import {Serie} from "./serie.js";
 import {series} from "./data.js";
 
 let datos: HTMLElement = document.getElementById("datosSeries")!;
+let carta: HTMLElement = document.getElementById("cartaInfo")!;
+let average: HTMLElement = document.getElementById("avg")!;
 
 cargarDatos(series);
+average.innerHTML = `<p>Seasons average: ${averageCalc(series)}</p> `;
 
 function cargarDatos(series: Serie[]) {
     series.forEach(value => {
-        let tr: HTMLElement = document.createElement('tr');
+        let tr: HTMLElement = document.createElement("tr");
+        tr.setAttribute("class", "clickable");
         tr.innerHTML = `
             <th scope="row">${value.getId()}</th>
             <td><a href="${value.getPaginaWeb()}" target="_blank">${value.getNombre()}</a></td>
@@ -15,6 +19,30 @@ function cargarDatos(series: Serie[]) {
             <td>${value.getTemporadas()}</td>
             `
         datos.appendChild(tr);
+
+        tr.onclick = function () {
+            cargarCarta(value);
+        };
     });
 }
 
+function averageCalc(series: Serie[]) {
+    let average: number = 0;
+    let cant: number = 0;
+    series.forEach(value => {
+        cant++;
+        average += value.getTemporadas();
+    });
+    return average/cant;
+}
+
+function cargarCarta(serie: Serie) {
+    carta.innerHTML = `
+        <div class="card">
+         <div class="card-header"><img src="${serie.getImagen()}" alt="${serie.getNombre()}" width="473" height=" "></div>
+         <div class="card-title">${serie.getNombre()}</div>
+         <div class="card-body">${serie.getInfo()}</div>
+         <div class="card-footer"><a href="${serie.getPaginaWeb()}" target="_blank">${serie.getPaginaWeb()}</a></div>
+        </div>
+        `
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
 import { series } from "./data.js";
 var datos = document.getElementById("datosSeries");
+var carta = document.getElementById("cartaInfo");
+var average = document.getElementById("avg");
 cargarDatos(series);
+average.innerHTML = "<p>Seasons average: ".concat(averageCalc(series), "</p> ");
 function cargarDatos(series) {
     series.forEach(function (value) {
-        var tr = document.createElement('tr');
+        var tr = document.createElement("tr");
+        tr.setAttribute("class", "clickable");
         tr.innerHTML = "\n            <th scope=\"row\">".concat(value.getId(), "</th>\n            <td><a href=\"").concat(value.getPaginaWeb(), "\" target=\"_blank\">").concat(value.getNombre(), "</a></td>\n            <td>").concat(value.getPlataforma(), "</td>\n            <td>").concat(value.getTemporadas(), "</td>\n            ");
         datos.appendChild(tr);
+        tr.onclick = function () {
+            cargarCarta(value);
+        };
     });
+}
+function averageCalc(series) {
+    var average = 0;
+    var cant = 0;
+    series.forEach(function (value) {
+        cant++;
+        average += value.getTemporadas();
+    });
+    return average / cant;
+}
+function cargarCarta(serie) {
+    carta.innerHTML = "\n        <div class=\"card\">\n         <div class=\"card-header\"><img src=\"".concat(serie.getImagen(), "\" alt=\"").concat(serie.getNombre(), "\" width=\"473\" height=\" \"></div>\n         <div class=\"card-title\">").concat(serie.getNombre(), "</div>\n         <div class=\"card-body\">").concat(serie.getInfo(), "</div>\n         <div class=\"card-footer\"><a href=\"").concat(serie.getPaginaWeb(), "\" target=\"_blank\">").concat(serie.getPaginaWeb(), "</a></div>\n        </div>\n        ");
 }
